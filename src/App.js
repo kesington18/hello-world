80aa4ef4ca18de8137c8898111bc1428193ff4f3
kesington <mr.abdulwakeel@yahoo.com>
2025-08-02 12:53:39 +0100

    Add name prop to HelloWorld component and update rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 
-function HelloWorld() {
+function HelloWorld({name}) {
   return(
     <main>
       <div className='card'>
-        <img src='' alt=''/>
+        <img className="header-img"
+          src="https://tse4.mm.bing.net/th/id/OIP.0ku1AvUo__-ohxH4EqTXOAHaEo?rs=1&pid=ImgDetMain&o=7&rm=3" 
+          alt=""/>
         <p className="tag">
           learning
         </p>
         <p>published 21 dec 2013</p>
+        <h1>HTML znd CSS Foundation</h1>
+        <p className='content'>These languages are the backbone of every website, defining content, structure, and presentation</p>
+
+        <div className='profile'>
+          <img src="/images/image-avatar.webp" alt='' />
+          <p>{name}</p>
+        </div>
         
       </div>
       
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelloWorld />
+    <HelloWorld name="Williams"/>
   </React.StrictMode>
 );
 
